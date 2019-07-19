ed7384c4a702dcb8bdeeb5763b20608b098a289a
Hanyinh <Hanyinh@qq.com>
2019-07-19 10:02:51 +0800

    vue实现

---
+++ src/components/Users.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <li v-for="(item, index) of users" :key="index">{{item}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  created () {
+    this.axios.get('/localhost/user/getUserList')
+      .then(res => {
+        this.users = res.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
+</script>
+
+<style scoped>
+</style>
